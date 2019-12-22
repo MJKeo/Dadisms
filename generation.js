2d87962b007dcebabc81b8ab8175db8843e7c819
Mike Keohane <mkeohane@gatech.edu>
2019-12-22 14:57:54 -0500

    added actual isms

--- generation.js
+++ generation.js
@@ -2,8 +2,15 @@ var output = document.getElementById("result");
 var button = document.getElementById("button");
 
 var phrases = [
-    "one",
-    "two"
+    "*someone dabs* \"Dib!",
+    "\"Ghost busters, were'nt those the guys on Discovery Channel?\"",
+    "\"Fing shwa\" (intended to be feng shui",
+    "\"Kernel Tunnel Syndrome\"",
+    "the quality of being impudent; impertinence",
+    "\"No it snot\"",
+    "Wiki: pliable twigs, typically of willow, plaited or woven to make items such as furniture and baskets",
+    "A common workout tool, the cowbell",
+    "christmas swag = \'swagger\'"
 ];
 
 
